ee92c65be44c08af070a1f3bdbf1acfc38719a29
rickyMio <ricoricky-26@hotmail.it>
2016-11-22 17:35:46 +0100

    Primo commit

---
+++ monster-csharp/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monster_csharp
+{
+    class Monster
+    {
+        public string name;
+        public int hp;
+        public int damage;
+
+        public Monster(string name, int hp, int damage)
+        {
+            this.name   = name;
+            this.hp     = hp;
+            this.damage = damage;
+            describe();
+        }
+
+        public void describe()
+        {
+            Console.WriteLine("Questo è " + name);
+            Console.WriteLine("HP: " + hp);
+            Console.WriteLine("DAMAGE: " + damage);
+        }
+    }
+}
